ef1a7adadc89ef1e52889ac88d457cbb5562f132
amoore3234 <akmoore@cpp.edu>
2023-04-26 08:41:45 -0700

    Add AprilSavings class

--- src/savings/AprilSavings.java
+++ src/savings/AprilSavings.java
@@ -1,42 +1,37 @@
 package savings;
 
-
-public class FebruarySavings extends Calculations {
-
-  public FebruarySavings() {
-  }
-
+public class AprilSavings extends Calculations {
   public double getAverageBalance(double highYieldPrinciple) {
     double averageBalance = 0;
     int days = 1;
     int daysInMonth = 0;
-    while (days <= 31) {
+    while (days <= 30) {
       if (days == 1) {
         daysInMonth = days;
         averageBalance = calculateDailyPrinciple(days, averageBalance, highYieldPrinciple);
       }
-      if (days == 2) {
+      if (days == 13) {
         days++;
         days -= daysInMonth;
         averageBalance = calculateDailyPrinciple(days, averageBalance, highYieldPrinciple);
-        highYieldPrinciple += 2392.44;
-        days = 2;
-        daysInMonth = 2;
+        highYieldPrinciple += 2392.43;
+        days = 13;
+        daysInMonth = 13;
       }
-      if (days == 16) {
+      if (days == 27) {
         days++;
         days -= daysInMonth;
         averageBalance = calculateDailyPrinciple(days, averageBalance, highYieldPrinciple);
-        highYieldPrinciple += 2392.44;
-        days = 16;
-        daysInMonth = 16;
+        highYieldPrinciple += 2392.45;
+        days = 27;
+        daysInMonth = 27;
       }
-      if (days == 28) {
+      if (days == 30) {
         days++;
         days -= daysInMonth;
         averageBalance = calculateDailyPrinciple(days, averageBalance, highYieldPrinciple);
         averageBalance += highYieldPrinciple;
-        days = 28;
+        days = 30;
       }
       days++;
     }
@@ -44,16 +39,16 @@ public class FebruarySavings extends Calculations {
   }
 
   public double getHighYieldPrinciple(double highYieldPrinciple) {
-    highYieldPrinciple += 2392.44;
-    highYieldPrinciple += 2392.44;
+    highYieldPrinciple += 2392.43;
+    highYieldPrinciple += 2392.45;
     return highYieldPrinciple;
   }
 
   public double getHighYieldInterest(double highYieldPrinciple, double averageBalance, double highYieldInterest) {
     double interest = 0.0;
     double interestFinal = 0.0;
-    double interestRate = 0.0393;
-    double monthInDays = 31;
+    double interestRate = 0.0422;
+    double monthInDays = 30;
     double daysInYears = 365;
     double averageDays = monthInDays / daysInYears;
     interest = averageDays * 12;
@@ -62,6 +57,5 @@ public class FebruarySavings extends Calculations {
     highYieldInterest = averageBalance * (Math.pow(1 + interestFinal, interest));
     highYieldInterest -= averageBalance;
     return highYieldInterest;
-  }
-    
+  } 
 }
